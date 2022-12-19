931e36af5427441938dd94fc877929083fdb37d6
Júlio Mendonça <juliommen@gmail.com>
2022-12-19 14:43:02 -0400

    kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
 import { DatabaseModule } from './infra/database/database.module';
+import { MessaginModule } from './infra/messaging/messaging.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule, MessaginModule],
 })
 export class AppModule {}
---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['glowing-moccasin-11952-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username:
+            'Z2xvd2luZy1tb2NjYXNpbi0xMTk1MiSrYR7pzykCRYixkkLJhMELAHxNa6J0hqE',
+          password:
+            'vDf0Wxu4ApvTAfaFzqJYLFEaadQBX_Aamgwu7tQllgSoBOO4nKJoj-5R7VNRyr70HqMZJw==',
+        },
+        ssl: true,
+      },
+    });
+  }
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
